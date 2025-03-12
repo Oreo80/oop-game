127e4222ec8b57aedebf1250b12e44d0c249133e
Oreo80 <Oreo80@users.noreply.github.com>
2025-03-12 19:36:29 +0200

    added ~movement~

--- main.cpp
+++ main.cpp
@@ -1,126 +1,88 @@
 #include <iostream>
-#include <array>
 #include <chrono>
-#include <thread>
+//#include <thread>
 
 #include <SFML/Graphics.hpp>
 
-#include <Helper.h>
-
-//////////////////////////////////////////////////////////////////////
-/// This class is used to test that the memory leak checks work as expected even when using a GUI
-class SomeClass {
+class Player {
+private:
+    sf::Texture texture{};
+    sf::Sprite sprite;
+    sf::Vector2f position;
 public:
-    explicit SomeClass(int) {}
+    explicit Player(const std::string& texturePath="../img/spr_heart_0.png", const sf::Vector2f& pos={312, 232})
+        : texture([&]() {
+            sf::Texture tex;
+            if (!tex.loadFromFile(texturePath)) {
+                std::cerr << "Failed to load sprite texture!\n";
+                throw std::runtime_error("Failed to load sprite texture");
+            }
+            return tex;
+        }()),
+          sprite(texture)
+    {
+        sprite.setPosition(pos);
+        position = pos;
+    }
+    void draw(sf::RenderWindow& window) const {
+        window.draw(sprite);
+    }
+    void setPosition(const sf::Vector2f& position) {
+        sprite.setPosition(position);
+    }
+
+    sf::Vector2f getPosition() const {
+        return sprite.getPosition();
+    }
 };
-
-SomeClass *getC() {
-    return new SomeClass{2};
-}
-//////////////////////////////////////////////////////////////////////
-
-
-int main() {
-    ///
-    std::cout << "Hello, world!\n";
-    std::array<int, 100> v{};
-    int nr;
-    std::cout << "Introduceți nr: ";
-    /////////////////////////////////////////////////////////////////////////
-    /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
-    /// dați exemple de date de intrare folosind fișierul tastatura.txt
-    /// Trebuie să aveți în fișierul tastatura.txt suficiente date de intrare
-    /// (în formatul impus de voi) astfel încât execuția programului să se încheie.
-    /// De asemenea, trebuie să adăugați în acest fișier date de intrare
-    /// pentru cât mai multe ramuri de execuție.
-    /// Dorim să facem acest lucru pentru a automatiza testarea codului, fără să
-    /// mai pierdem timp de fiecare dată să introducem de la zero aceleași date de intrare.
-    ///
-    /// Pe GitHub Actions (bife), fișierul tastatura.txt este folosit
-    /// pentru a simula date introduse de la tastatură.
-    /// Bifele verifică dacă programul are erori de compilare, erori de memorie și memory leaks.
-    ///
-    /// Dacă nu puneți în tastatura.txt suficiente date de intrare, îmi rezerv dreptul să vă
-    /// testez codul cu ce date de intrare am chef și să nu pun notă dacă găsesc vreun bug.
-    /// Impun această cerință ca să învățați să faceți un demo și să arătați părțile din
-    /// program care merg (și să le evitați pe cele care nu merg).
-    ///
-    /////////////////////////////////////////////////////////////////////////
-    std::cin >> nr;
-    /////////////////////////////////////////////////////////////////////////
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "v[" << i << "] = ";
-        std::cin >> v[i];
-    }
-    std::cout << "\n\n";
-    std::cout << "Am citit de la tastatură " << nr << " elemente:\n";
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "- " << v[i] << "\n";
-    }
-    ///////////////////////////////////////////////////////////////////////////
-    /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
-    /// alt fișier propriu cu ce alt nume doriți.
-    /// Exemplu:
-    /// std::ifstream fis("date.txt");
-    /// for(int i = 0; i < nr2; ++i)
-    ///     fis >> v2[i];
-    ///
-    ///////////////////////////////////////////////////////////////////////////
-    ///                Exemplu de utilizare cod generat                     ///
-    ///////////////////////////////////////////////////////////////////////////
-    Helper helper;
-    helper.help();
-    ///////////////////////////////////////////////////////////////////////////
-
-    SomeClass *c = getC();
-    std::cout << c << "\n";
-    delete c;  // comentarea acestui rând ar trebui să ducă la semnalarea unui mem leak
-
+class Game {
+private:
     sf::RenderWindow window;
-    ///////////////////////////////////////////////////////////////////////////
-    /// NOTE: sync with env variable APP_WINDOW from .github/workflows/cmake.yml:31
-    window.create(sf::VideoMode({800, 700}), "My Window", sf::Style::Default);
-    ///////////////////////////////////////////////////////////////////////////
-    std::cout << "Fereastra a fost creată\n";
-    ///////////////////////////////////////////////////////////////////////////
-    /// NOTE: mandatory use one of vsync or FPS limit (not both)            ///
-    /// This is needed so we do not burn the GPU                            ///
-    window.setVerticalSyncEnabled(true);                                    ///
-    /// window.setFramerateLimit(60);                                       ///
-    ///////////////////////////////////////////////////////////////////////////
-
-    while(window.isOpen()) {
-        bool shouldExit = false;
-
-        while(const std::optional event = window.pollEvent()) {
-            if (event->is<sf::Event::Closed>()) {
-                window.close();
-                std::cout << "Fereastra a fost închisă\n";
-            }
-            else if (event->is<sf::Event::Resized>()) {
-                std::cout << "New width: " << window.getSize().x << '\n'
-                          << "New height: " << window.getSize().y << '\n';
-            }
-            else if (event->is<sf::Event::KeyPressed>()) {
-                const auto* keyPressed = event->getIf<sf::Event::KeyPressed>();
-                std::cout << "Received key " << (keyPressed->scancode == sf::Keyboard::Scancode::X ? "X" : "(other)") << "\n";
-                if(keyPressed->scancode == sf::Keyboard::Scancode::Escape) {
-                    shouldExit = true;
+    Player player;
+    void handle_events() {
+            bool shouldExit = false;
+            while(const std::optional event = window.pollEvent()) {
+                if (event->is<sf::Event::Closed>()) {
+                    window.close();
+                }
+                if (event->is<sf::Event::KeyPressed>()) {
+                    const auto* keyPressed = event->getIf<sf::Event::KeyPressed>();
+                    if(keyPressed->scancode == sf::Keyboard::Scancode::Escape) {
+                        shouldExit = true;
+                    }
+                    if(keyPressed->scancode == sf::Keyboard::Scancode::Up) {player.setPosition(player.getPosition() + sf::Vector2f(0, -1));}
+                    if(keyPressed->scancode == sf::Keyboard::Scancode::Down) {player.setPosition(player.getPosition() + sf::Vector2f(0, 1));}
+                    if(keyPressed->scancode == sf::Keyboard::Scancode::Left) {player.setPosition(player.getPosition() + sf::Vector2f(-1, 0));}
+                    if(keyPressed->scancode == sf::Keyboard::Scancode::Right) {player.setPosition(player.getPosition() + sf::Vector2f(1, 0));}
                 }
             }
-        }
-        if(shouldExit) {
-            window.close();
-            std::cout << "Fereastra a fost închisă (shouldExit == true)\n";
-            break;
-        }
-        using namespace std::chrono_literals;
-        std::this_thread::sleep_for(300ms);
-
+            if(shouldExit) {
+                window.close();
+            }
+    }
+    void render() {
         window.clear();
+        player.draw(window);
         window.display();
     }
-
+public:
+    Game() : window(sf::VideoMode({640, 480}), "My Window", sf::Style::Titlebar | sf::Style::Close), player() {
+        window.setFramerateLimit(30);
+    }
+    void run() {
+        while (window.isOpen()) {
+            handle_events();
+            render();
+        }
+    }
+};
+int main() {
+    try {
+        Game game;
+        game.run();
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << '\n';
+    }
     std::cout << "Programul a terminat execuția\n";
     return 0;
 }
