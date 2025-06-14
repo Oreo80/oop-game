524d60f9986ea59bf6942f0a57176ab726981189
Oreo80 <Oreo80@users.noreply.github.com>
2025-06-14 12:19:38 +0300

    doamne ajuta

--- Headers/DrawableEntity.h
+++ Headers/DrawableEntity.h
@@ -15,6 +15,7 @@ public:
     [[nodiscard]] virtual sf::Vector2f getPosition() const = 0;
     virtual void setPosition(const sf::Vector2f& pos) = 0;
     [[nodiscard]] virtual sf::FloatRect getGlobalBounds() const = 0;
+    // cppcheck-suppress unusedFunction
     [[nodiscard]] virtual std::unique_ptr<DrawableEntity> clone() const = 0;
     virtual ~DrawableEntity() = default;
     friend std::ostream& operator<<(std::ostream &os, const DrawableEntity& obj) {
